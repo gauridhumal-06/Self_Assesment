e5cdcf97df51692fa118fab3d3043d4c4ac57309
gauridhumal-06 <106648403+gauridhumal-06@users.noreply.github.com>
2022-12-12 13:19:41 +0530

    Update Que-02.c.sh

--- Self_Assesment/Operating System/Que-02.c.sh
+++ Self_Assesment/Operating System/Que-02.c.sh
@@ -1,8 +1,15 @@
+# // reading a string
+# // using via user input
 s=$1
+# // getting the length of given string
 # s="Hello World"
 strlen=${#s}
+# // looping for reversing a string
+# // initialize i=len-1 for reversing a string and run till i=0
+# // printing in the reverse order of the given string
 for (( i=$strlen-1; i>=0; i-- ));
 do
+#  // "${string:$i:1}"extract single character from string
     revstr=$revstr${s:$i:1}
 done
 echo "Original String : $s"
